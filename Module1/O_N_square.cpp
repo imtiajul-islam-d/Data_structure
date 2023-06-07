4e8abbb54d61062fc2bf8efb926d6cc6cd02de65
imtiajul-islam-d <imtiajul@hotmail.com>
2023-06-07 18:30:11 +0600

    complexity

---
+++ Module1/O_N_square.cpp
@@ -0,0 +1,17 @@
+// O(n ^ 3) ^ 2  = O(n6)
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 10; i++) // O(10 ^ 2);
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            
+        }
+        
+    }
+    
+    return 0;
+}
